7855bfc704c89388cb743b6fafa2af6fb469a076
Michelangelo Stega <mikybeeh@hotmail.it>
2025-08-26 10:21:36 +0200

    added fullscreen mode (commented) in __init__ method;

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
+        
     def run_game(self):
         '''Avvia il ciclo principale del gioco'''
         while True:
