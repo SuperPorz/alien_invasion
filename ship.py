40d5a3929173717a0ef1f87b499e876a1b0503c5
Michelangelo Stega <mikybeeh@hotmail.it>
2025-08-26 09:55:08 +0200

    implemented a continuos movement for the ship;

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         '''Inizializza la nave e ne imposta la posizione iniziale'''
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         # carica l'immagine della nave e ne ottiene il rettangolo
         self.image = pygame.image.load('images/spaceships/bgbattleship.bmp')
@@ -15,16 +16,23 @@ class Ship:
         # avvia ogni nuova nave in fondo allo schermo, al centro (posizione iniziale)
         self.rect.midbottom = self.screen_rect.midbottom
         
+        # memorizza un float per la posizione orizzontale esatta della nave
+        self.x = float(self.rect.x)
+        
         # flag del movimento; inizia con una nave che non si muove
         self.moving_right = False
         self.moving_left = False
         
     def update(self):
         '''Aggiorna la posizione della nave in base al flag del movimento'''
+        # aggiorna il valore x della nave, non il rect()
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # aggiorna l'oggetto rect() da self.x
+        self.rect.x = self.x
     
     def blitme(self):
         '''Disegna la nave nella sua posizione corrente'''
